74c8d3b59ffb6aec509152600b3d52fd6dce2c72
Misay1125 <14862208+misay1125@user.noreply.gitee.com>
2025-12-06 15:27:40 +0800

    update-Crypto-Source-Code

--- Crypto/AnyPointAnyCurve/src/main.py
+++ Crypto/AnyPointAnyCurve/src/main.py
@@ -8,8 +8,8 @@ import socket
 from random import randrange
 from Crypto.Hash import SHA256
 
-flag = os.getenv("GZCTF_FLAG")
-
+# flag = os.getenv("GZCTF_FLAG")
+flag = "whuctf{Ch3ck_7he_Curv3_1s_Very_Imp0rtant!}"
 p = 0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff
 a = 0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc
 b = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
